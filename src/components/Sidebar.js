8c3a377558692ef6f762b8097bc9470ed03732af
Dio Fitriadi <fitriadidio@gmail.com>
2022-12-11 16:45:36 +0700

    fix bug

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,22 +5,22 @@ const Sidebar = () => {
   return (
     <aside className="h-screen sticky top-0" aria-label="Sidebar">
       <div className="overflow-y-auto py-4 px-3 h-screen bg-[#1F1D2B]">
-        <div className='flex flex-col justify-center items-center w-20 h-full'>
+        <div className='flex flex-col justify-center items-center w-20 h-full gap-10'>
           <button>
-            <img src="/img/sidebar/Shop.svg" className='w-[56px]' alt="shop" />
+            <img src="/img/sidebar/Shop.svg" className='w-[56px] ' alt="shop" />
           </button>
           <Link to='/'>
-            <img className='ml-1 w-[200px]' src="/img/sidebar/Home.svg" alt="shop" />
+            <img className='w-[25px] transform hover:scale-150 ' src="/img/sidebar/home.svg" alt="shop" />
           </Link>
           <Link to='/dashboard'>
-            <img className='ml-1 w-[200px]' src="/img/sidebar/Pie.svg" alt="shop" />
+            <img className='w-[25px] transform hover:scale-150' src="/img/sidebar/pie.svg" alt="shop" />
           </Link>
           <Link to='/settings'>
-            <img className='ml-1 w-[200px]' src="/img/sidebar/Settings.svg" alt="shop" />
+            <img className='w-[25px] transform hover:scale-150' src="/img/sidebar/settings.svg" alt="shop" />
           </Link>
           <div className='flex flex-col items-center justify-end h-full'>
             <button>
-              <img className='ml-1 w-[150px]' src="/img/sidebar/Logout.svg" alt="shop" />
+              <img className='w-[150px]' src="/img/sidebar/logout.svg" alt="shop" />
             </button>
           </div>
         </div>
