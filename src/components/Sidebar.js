333e77fdea1e0e5b9d307b9bee44cfacf244e5e1
Dio Fitriadi <fitriadidio@gmail.com>
2022-12-16 21:15:29 +0700

    fix bug

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -24,14 +24,14 @@ const Sidebar = () => {
   return (
     <aside className="h-screen sticky top-0" aria-label="Sidebar">
       <div className="overflow-y-auto py-4 px-3 h-screen bg-[#1F1D2B]">
-        <div className='flex flex-col items-center w-20 h-full gap-4'>
+        <div className='flex flex-col justify-center items-center w-20 h-full gap-4'>
           <button>
             <img src="/img/sidebar/Shop.svg" className='w-[56px] ' alt="shop" />
           </button>
           <button>
             {pathname === '/' ? (
               <Link to='/'>
-                <div className='w-full bg-[#252836] pr-4 ml-4 rounded-l-3xl'>
+                <div className='w-full bg-[#252836] pr-3 ml-[7px] rounded-l-3xl'>
                   <div className='w-[70px] h-[70px] flex justify-center items-center '>
                     <img className='w-[45px] p-3 bg-[#FFCA40]' src="/img/sidebar/home-white.svg" alt="shop" />
                   </div>
@@ -39,18 +39,17 @@ const Sidebar = () => {
               </Link>
             ) : (
               <Link to='/'>
-                <div className='w-full h-[70px] flex items-center justify-center'>
+                <div className='w-[70px] h-[70px] flex items-center justify-center'>
                   <img className='w-[35px] p-2' src="/img/sidebar/home.svg" alt="shop" />
                 </div>
               </Link>
             )
             }
           </button>
-
           <button>
             {pathname === '/dashboard' ? (
               <Link to='/dashboard'>
-                <div className='w-full bg-[#252836] pr-4 ml-4 rounded-l-3xl'>
+                <div className='w-full bg-[#252836] pr-3 ml-[7px] rounded-l-3xl'>
                   <div className='w-[70px] h-[70px] flex justify-center items-center '>
                     <img className='w-[45px] p-3 bg-[#FFCA40]' src="/img/sidebar/pie-white.svg" alt="shop" />
                   </div>
@@ -58,7 +57,7 @@ const Sidebar = () => {
               </Link>
             ) : (
               <Link to='/dashboard'>
-                <div className='w-full h-[70px] flex items-center justify-center rounded-tr-xl'>
+                <div className='w-[70px] h-[70px] flex items-center justify-center '>
                   <img className='w-[35px] p-2' src="/img/sidebar/pie.svg" alt="shop" />
                 </div>
               </Link>
@@ -68,7 +67,7 @@ const Sidebar = () => {
           <button >
             {pathname === '/settings' ? (
               <Link to='/settings'>
-                <div className='w-full bg-[#252836] pr-4 ml-4 pl-2 rounded-l-3xl'>
+                <div className='w-full bg-[#252836] pr-3 ml-[7px] rounded-l-3xl'>
                   <div className='w-[70px] h-[70px] flex justify-center items-center '>
                     <img className='w-[45px] p-3 bg-[#FFCA40]' src="/img/sidebar/settings-white.svg" alt="shop" />
                   </div>
